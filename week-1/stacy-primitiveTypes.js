6b9b9786f7a546c8d82d679c263a3196fbdd9128
Clayton Stacy <clayton.stacy@gmail.com>
2019-11-24 17:58:38 -0600

    Finishing exercise 1.4

---
+++ week-1/stacy-primitiveTypes.js
@@ -0,0 +1,8 @@
+var firstName = "Clayton";
+var lastName = "Stacy";
+var age = "43";
+var street = "2384 South Robin Lane";
+var city = "Hotlanta";
+var zipCode = "12345";
+
+console.log(`${firstName}, ${lastName}, ${age}, ${street}, ${city}, ${zipCode}`);
