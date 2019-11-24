23b279d92d5fc53d538f5e02d4a950665efbb798
Clayton Stacy <clayton.stacy@gmail.com>
2019-11-24 17:31:49 -0600

    Adding hello world demo js file

---
+++ week-1/hello-world.js
@@ -0,0 +1,10 @@
+/*
+============================================
+; Title: Assignment 1.3
+; Author: Professor Massoud
+; Date: 24 November 2019
+; Modified By: Clayton Stacy
+; Description: Hello World Demo
+*/ 
+
+console.log("Hello World, my name is Clayton Stacy"); 
