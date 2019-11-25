2ff5fbcfdb0e7298937c0e5577c99c35248fbc18
Clayton Stacy <clayton.stacy@gmail.com>
2019-11-25 11:36:47 -0600

    Starting Ex 1.1

---
+++ week-1/stacy-1.1valuesAndVariables.js
@@ -0,0 +1,17 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Clayton Stacy
+; Date: November 2019
+; Modified By: Clayton Stacy
+; Description: Find the two errors
+============================================
+*/ 
+
+var date = '13/1/2019';
+var newDate = new Date(date).toLocaleDateString();
+var b = π;
+var c = 50;
+
+console.log(newDate);
+console.log(b * c);
