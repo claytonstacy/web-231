6945c4497cbfe8bd34f7a1abea1abf9a4c358afc
Clayton Stacy <clayton.stacy@gmail.com>
2019-12-01 15:15:47 -0600

    Cleaning up comments

--- week-2/Stacy-function-properties.js
+++ week-2/Stacy-function-properties.js
@@ -1,10 +1,10 @@
 /*
 ============================================
-; Title:  header.js
+; Title:  Stacy-function-properties.js
 ; Author: Clayton Stacy
 ; Date:   1 December 2019
 ; Modified By: Clayton Stacy
-; Description: Exploring creating functions
+; Description: Exploring function properties
 ;===========================================
 */
 
@@ -12,11 +12,11 @@
 myName.first = "Clayton";
 myName.last = "Stacy"
 
-//Building the function
+//Building the function, building array to return both values
 function myName() {
     const fullName = [myName.first, myName.last];
     return fullName;
 };
 
-//Logging function to the console
+//Logging function to the console after joining the returned array with a space between each element
 console.log(`Hello ${myName().join(" ")}`);
