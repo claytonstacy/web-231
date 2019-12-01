118e04c21c56ff75fb3bf2c31e17ee852c2d8a37
Clayton Stacy <clayton.stacy@gmail.com>
2019-12-01 14:20:27 -0600

    Finishing 2.2

---
+++ week-2/Stacy-header-test.js
@@ -0,0 +1,7 @@
+
+//Importing header file
+const header = require('./Stacy-header.js');
+
+//Invoking display function from the header file, logging to console
+
+console.log(header.display('Clayton', 'Stacy', '2.2 Header'));
