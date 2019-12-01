fda4b4867fad428aa338156278190d2e9a53c1fc
Clayton Stacy <clayton.stacy@gmail.com>
2019-12-01 14:26:15 -0600

    Clean up

--- week-2/Stacy-header-test.js
+++ week-2/Stacy-header-test.js
@@ -12,5 +12,4 @@
 const header = require('./Stacy-header.js');
 
 //Invoking display function from the header file, logging to console
-
 console.log(header.display('Clayton', 'Stacy', '2.2 Header'));
