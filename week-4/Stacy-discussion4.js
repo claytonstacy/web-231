97a9f7f4ce8095a44de5659e0a0c080905775551
Clayton Stacy <clayton.stacy@gmail.com>
2019-12-19 18:52:16 -0600

    Adding comments

--- week-4/Stacy-discussion4.js
+++ week-4/Stacy-discussion4.js
@@ -12,10 +12,13 @@
 var header = require('../week-2/Stacy-header.js');
 console.log(header.display('Clayton', 'Stacy', '3.2 Pattern Matching Functions'));
 
+//Declare the array teams with teams
 const teams = 'Steelers, Cowboys, Eagles, Ravens, Giants, Browns';
 
+//Iterate through the array and sort them into divisions
 teams.forEach(teams => {
     if (team === 'Browns' || team === 'Steelers' || team === 'Ravens') {
+        //Log the AFC North teams using template literals
         console.log(`The ${ team } play in the AFC North`)
     } else {
         console.log(`The ${ team } play in the NFC East`)
